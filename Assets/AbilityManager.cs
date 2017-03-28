2e22dcc2516bc832ce41e878fe953769299d5702
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-28 16:42:52 -0500

    Added leap compatability

---
+++ Assets/AbilityManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityManager : MonoBehaviour
+{
+    public void CastAntiVirus()
+    {
+        print("Casting Anti Virus");
+    }
+
+    public void CastQuarentine()
+    {
+        print("Casting Quarentine");
+    }
+
+    public void CastFireWall()
+    {
+        print("Casting FireWall");
+    }
+}
