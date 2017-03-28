3fdad4fcddf42e51debae9685c9ac7a6a13dc758
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-28 14:42:02 -0500

    added some art, added leap stuff

---
+++ Assets/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+}
